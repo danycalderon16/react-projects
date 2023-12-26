34f8997ef4b6e5e2cfda0939d030581cf2d79e0b
Daniel Calderon <danycalvir@gmail.com>
2023-12-25 23:07:52 -0700

    refs

--- 08-portals/src/components/Player.jsx
+++ 08-portals/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const palyerName = useRef();
+  const [enteredPlayerName, setenteredPlayerName] = useState(null);
+
+  function handleSubmit(){
+    setenteredPlayerName(palyerName.current.value);
+    palyerName.current.value = '';
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+        ref={palyerName} 
+        type="text" />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
