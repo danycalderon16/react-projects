79d232a64a1c9001595fd5061b835f3701a63614
Daniel Calderon <danycalvir@gmail.com>
2024-01-02 22:59:35 -0700

    finish project 09

--- 09-practice-project/src/components/NewProject.jsx
+++ 09-practice-project/src/components/NewProject.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react'
-import Input from './Input'
+import React, { useRef } from "react";
+import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd,onCancel ,}) {
+  const modalRef = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -10,22 +12,44 @@ export default function NewProject({onAdd}) {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
-    
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modalRef.current.open();
+      return;
+    }
+
     onAdd({
-      title:enteredTitle,
-      description:enteredDescription,
-      dueDate:enteredDueDate,
-      }
-    )
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
   }
 
   return (
-    <div className='w-[35rem] mt-16'>
-     <menu className="flex items-center justify-end gap-4 my-4">
+    <>
+      <Modal ref={modalRef} buttonCaption="Okay">
+        <>
+          <h2 className="text-xl font-bold text-stone-700 my-4">
+            Invalid Input
+          </h2>
+          <p className="text-stone-600 mb-4">
+            Oops ... looks like you forgot to enter a value.
+          </p>
+          <p className="text-stone-600 mb-4">
+            Please make sure you provide a valid value for every input field.
+          </p>
+        </>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
           <li>
             <button
               className="text-stone-800 hover:text-stone-950"
-              // onClick={onCancel}
+              onClick={onCancel}
             >
               Cancel
             </button>
@@ -39,11 +63,12 @@ export default function NewProject({onAdd}) {
             </button>
           </li>
         </menu>
-      <div>
-       <Input type="text" label="Title" ref={title}/>
-       <Input label="Description" ref={description} textarea/>
-       <Input label="Due date" ref={dueDate}/>
+        <div>
+          <Input type="text" label="Title" ref={title} />
+          <Input label="Description" ref={description} textarea />
+          <Input type="date" label="Due date" ref={dueDate} />
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
