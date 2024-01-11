02d5a3ce3f65e2561238bfbec121fdfa44142dd5
Daniel Calderon <danycalvir@gmail.com>
2024-01-10 22:31:06 -0700

    get values from form

--- 17-forms/src/components/Login.jsx
+++ 17-forms/src/components/Login.jsx
@@ -1,17 +1,51 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: ""
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredValues);
+  }
+
+  function handleChange(event, identifier) {
+    setEnteredValues((prevValues) => {
+      return {
+        ...prevValues,
+        [identifier]: event.target.value
+      } 
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(e)=>handleChange(e, "email")}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(e)=>handleChange(e, "password")}
+          />
         </div>
       </div>
 
