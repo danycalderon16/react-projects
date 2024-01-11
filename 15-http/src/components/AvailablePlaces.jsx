2f46af9dd1cd3c360342a8a8c8ab69610c100ee5
Daniel Calderon <danycalvir@gmail.com>
2024-01-10 21:56:16 -0700

    finish custom hooks

--- 15-http/src/components/AvailablePlaces.jsx
+++ 15-http/src/components/AvailablePlaces.jsx
@@ -3,38 +3,32 @@ import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../../http.js";
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+    const places = await fetchAvailablePlaces();
+
+    return new Promise((resolve) => {
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(
+          places,
+          position.coords.latitude,
+          position.coords.longitude
+          );
+          resolve(sortedPlaces)
+        });
+      })
+}
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
 
-  useEffect(() => {
-    const fecthPlaces = async () => {
-      setError(null);
-      setIsFetching(true);
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message || "Something went wrong. Please try again later.",
-        });
-      } finally {
-        setIsFetching(false);
-      }
-    };
-    fecthPlaces();
-  }, []);
-
+  const {data:availablePlaces, 
+    isFetching, 
+    error} = 
+  useFetch(fetchSortedPlaces, []);
+  
   if (error) {
     return <Error title="An error ecurred!" message={error.message} />;
   }
